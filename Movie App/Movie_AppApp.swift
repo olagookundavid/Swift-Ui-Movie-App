c44c1f212ddd0cbde69908ac612abbe2f6ece223
David Olagookun <erijesudo@gmail.com>
2023-06-01 17:37:36 +0100

    Initial Commit

---
+++ Movie App/Movie_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_AppApp.swift
+//  Movie App
+//
+//  Created by David OH on 01/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Movie_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
